e88ff1b102bc88ab116dc4513abd2b8356b14d7f
Benj Ledesma <benj.ledesma@gmail.com>
2021-07-31 16:28:49 +0800

    Remove blog route

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,16 +17,6 @@
     <li>
       <a aria-current={isCurrentPage(segment, 'form')} href="form">form</a>
     </li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li>
-      <a
-        rel="prefetch"
-        aria-current={isCurrentPage(segment, 'blog')}
-        href="blog">blog</a
-      >
-    </li>
   </ul>
 </nav>
 
