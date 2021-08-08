c5785806b43b1085354761206b1bf85897674391
Benj Ledesma <benj.ledesma@gmail.com>
2021-08-08 18:24:23 +0800

    Create simple product update implementation

--- src/routes/api/product/index.ts
+++ src/routes/api/product/index.ts
@@ -58,3 +58,63 @@ export async function post(req: Request<Locals>): Promise<EndpointOutput> {
     }
   }
 }
+
+export async function put(req: Request<Locals>): Promise<EndpointOutput> {
+  const { seller } = req.locals
+  if (!seller) {
+    return {
+      status: 403
+    }
+  }
+  const { id: productId, ...rawUpdateBody } = req.body as any
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId
+      },
+      rejectOnNotFound: true
+    })
+    if (product.sellerId !== seller.id) {
+      throw new Error('Product does not exist under your seller account.')
+    }
+  } catch (err) {
+    return {
+      status: 404,
+      body: {
+        message: err.message
+      }
+    }
+  }
+
+  const updateBody: Partial<Product> = {}
+  if (rawUpdateBody.description) {
+    updateBody.description = rawUpdateBody.description
+  }
+  const select: Partial<Record<keyof Product, boolean>> = {}
+  for (let key in updateBody) {
+    select[key as keyof Product] = true
+  }
+
+  try {
+    const updatedProduct = await prisma.product.update({
+      select,
+      where: {
+        id: productId
+      },
+      data: updateBody
+    })
+    return {
+      body: {
+        data: updatedProduct
+      }
+    }
+  } catch (err) {
+    console.error(err)
+    return {
+      status: 400,
+      body: {
+        message: err.message
+      }
+    }
+  }
+}
--- src/routes/manage/product/[productId].svelte
+++ src/routes/manage/product/[productId].svelte
@@ -1,25 +1,71 @@
 <script lang="ts" context="module">
   import type { Load } from '@sveltejs/kit'
-  export const load: Load = async ({ page, fetch }) => {
+  export const load: Load = async ({ page, fetch, session }) => {
     const res = await fetch(`/api/product?id=${page.params.productId}`).then(
       (r) => r.json()
     )
-    const { id, name } = res.data
+    const product = res.data
+    // product should be owned by seller
+    if (session.seller.id !== product.sellerId) {
+      return {
+        status: 403
+      }
+    }
     return {
       props: {
-        id,
-        name
+        id: product.id,
+        name: product.name,
+        description: product.description ?? ''
       }
     }
   }
 </script>
 
 <script lang="ts">
+  import axios from 'axios'
+
   export let id: string
   export let name: string
+  export let description: string
+
+  let formData = {
+    id,
+    name,
+    description
+  }
+  let submissionError = ''
+  async function onSubmit() {
+    const res = await axios.put('/api/product', formData)
+    console.log(res)
+  }
 </script>
 
-<h2>{name}</h2>
+<form action="submit" on:submit|preventDefault={onSubmit}>
+  <label for="name">name</label>
+  <input name="name" bind:value={formData.name} />
+  <label for="description">description</label>
+  <textarea name="description" bind:value={formData.description} />
 
-<style>
+  <br />
+  <button class="submit" type="submit">save</button>
+  {#if submissionError}
+    <span class="error mt-4">Error: {submissionError}</span>
+  {/if}
+</form>
+
+<style lang="postcss">
+  form {
+    @apply flex flex-col max-w-md;
+  }
+  input,
+  textarea {
+    @apply border rounded px-2 py-1 mb-2 text-sm;
+  }
+
+  .submit {
+    @apply rounded bg-orange-300 w-max py-1 px-5 hover:bg-orange-500;
+  }
+  .error {
+    @apply text-red-600 text-xs mb-1;
+  }
 </style>
