8937219f51dbf445ef736c255e6535da37de7b69
Benj Ledesma <benj.ledesma@gmail.com>
2021-08-08 17:37:29 +0800

    Add layout to /manage routes

---
+++ src/routes/manage/__layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  import { session } from '$app/stores'
+  import { onMount } from 'svelte'
+
+  $: authorized = !!$session.seller
+
+  onMount(() => {
+    if (!authorized) {
+      goto('/login')
+    }
+  })
+</script>
+
+<svelte:head>
+  <title>Manage</title>
+</svelte:head>
+
+{#if authorized}
+  <slot />
+{/if}
+
+<style>
+</style>
--- src/routes/manage/index.svelte
+++ src/routes/manage/index.svelte
@@ -1,31 +1,15 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
-  import { session } from '$app/stores'
-  import { onMount } from 'svelte'
-
-  $: authorized = !!$session.seller
-
-  onMount(() => {
-    if (!authorized) {
-      goto('/login')
-    }
-  })
+  import { session } from '$lib/stores'
 </script>
 
-<svelte:head>
-  <title>Manage</title>
-</svelte:head>
-
-{#if authorized}
-  <h1>Manage</h1>
-  <p>You're a seller!</p>
-  <a
-    class="underline hover:text-orange-500 duration-75"
-    href={`/u/${$session.seller.username}`}
-  >
-    My page
-  </a>
-{/if}
+<h1>Manage</h1>
+<p>You're a seller!</p>
+<a
+  class="underline hover:text-orange-500 duration-75"
+  href={`/u/${$session.seller.username}`}
+>
+  My page
+</a>
 
 <style>
 </style>
