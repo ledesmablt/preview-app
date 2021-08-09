5dcd3eb72e76adc3e241538c00c5f21805c7eeba
Benj Ledesma <benj.ledesma@gmail.com>
2021-08-09 13:02:48 +0800

    Add prisma time logging middleware (dev only)

--- src/lib/services/prisma.ts
+++ src/lib/services/prisma.ts
@@ -11,4 +11,17 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const prisma = new PrismaClient()
+
+if (process.env.NODE_ENV === 'development') {
+  // dev timer middleware
+  prisma.$use(async (params, next) => {
+    const timestamp = new Date().getTime().toString().slice(11)
+    const label = `prisma ${timestamp} ${params.action} ${params.model}`
+    console.time(label)
+    const result = await next(params)
+    console.timeEnd(label)
+    return result
+  })
+}
+
 export default prisma
