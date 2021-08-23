022e4a31ca87c4842e09e447f3703ab1958d6de4
Benj Ledesma <benj.ledesma@gmail.com>
2021-08-23 23:41:09 +0800

    Use houdini for product page

--- src/routes/product/[productId].svelte
+++ src/routes/product/[productId].svelte
@@ -1,61 +1,47 @@
 <script lang="ts" context="module">
-  import type { Load } from '@sveltejs/kit'
-  export const load: Load = async ({ page, fetch }) => {
-    const id = page.params.productId
-    const res = await fetch('/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        variables: { id, fromShop: true },
-        query: `query ($id: String!, $fromShop: Boolean!) {
-          get_product(id: $id, fromShop: $fromShop) {
-            id
-            name
-            description
-            price
-            currency
-            imageUrl
-            audioPreviewUrl
-            seller {
-              id
-              username
-              password
-              email
-              bio
-              userImageUrl
-            }
-          }
-        }`
-      })
-    }).then((r) => r.json())
-    if (res.errors) {
-      return {
-        status: 400,
-        error: new Error(res.errors[0].message)
-      }
-    }
-    const product = res.data.get_product
-    if (!product) {
-      return {
-        status: 404,
-        error: new Error(`Product ${id} not found`)
-      }
-    }
-    const { seller } = product
+  import type { LoadInput } from '@sveltejs/kit'
+  import type { ProductPageQuery$input } from '$houdini'
+  export function ProductPageQueryVariables({
+    page
+  }: LoadInput): ProductPageQuery$input {
     return {
-      props: {
-        product,
-        seller
-      }
+      id: page.params.productId,
+      fromShop: true
     }
   }
 </script>
 
 <script lang="ts">
-  export let product: any
-  export let seller: any
+  import { query, graphql } from '$houdini'
+  import type { ProductPageQuery } from '$houdini'
+  const { data: queryData } = query<ProductPageQuery>(graphql`
+    query ProductPageQuery($id: String!, $fromShop: Boolean!) {
+      get_product(id: $id, fromShop: $fromShop) {
+        id
+        name
+        description
+        price
+        currency
+        imageUrl
+        audioPreviewUrl
+        seller {
+          id
+          username
+          password
+          email
+          bio
+          userImageUrl
+        }
+      }
+    }
+  `)
+  if (!$queryData.get_product?.id) {
+    throw new Error('Product not found')
+  }
+
+  let { seller: querySeller, ...queryProduct } = $queryData.get_product!
+  let product = { ...queryProduct }
+  let seller = { ...querySeller }
 </script>
 
 <svelte:head>
@@ -65,7 +51,11 @@
 <h1 class="text-2xl">{product.name}</h1>
 <h2>by {seller.username}</h2>
 
-<img class="max-w-lg my-4" src={product.imageUrl} alt={product.name} />
+<img
+  class="max-w-lg my-4"
+  src={product.imageUrl || undefined}
+  alt={product.name || 'product'}
+/>
 
 <p>{product.price} {product.currency}</p>
 <p>{product.description}</p>
